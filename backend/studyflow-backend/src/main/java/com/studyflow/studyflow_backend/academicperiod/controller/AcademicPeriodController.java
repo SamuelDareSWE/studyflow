e7f3fc6555b63b851f2d3e5f0183744f3731357d
Samuel Oluwadare <techvalueiq@gmail.com>
2026-07-25 20:15:23 +0100

    Implement AcademicPeriod creation API with Spring Security configuration

--- backend/studyflow-backend/src/main/java/com/studyflow/studyflow_backend/academicperiod/controller/AcademicPeriodController.java
+++ backend/studyflow-backend/src/main/java/com/studyflow/studyflow_backend/academicperiod/controller/AcademicPeriodController.java
@@ -1,4 +1,25 @@
 package com.studyflow.studyflow_backend.academicperiod.controller;
 
+import com.studyflow.studyflow_backend.academicperiod.dto.AcademicPeriodResponse;
+import com.studyflow.studyflow_backend.academicperiod.dto.CreateAcademicPeriodRequest;
+import com.studyflow.studyflow_backend.academicperiod.service.AcademicPeriodService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/academic-periods")
 public class AcademicPeriodController {
+
+    private final AcademicPeriodService academicPeriodService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AcademicPeriodResponse createAcademicPeriod(
+           @Valid @RequestBody CreateAcademicPeriodRequest request){
+        return academicPeriodService.createAcademicPeriod(request);
+    }
+
 }
--- backend/studyflow-backend/src/main/java/com/studyflow/studyflow_backend/academicperiod/service/AcademicPeriodService.java
+++ backend/studyflow-backend/src/main/java/com/studyflow/studyflow_backend/academicperiod/service/AcademicPeriodService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 
 
 public interface AcademicPeriodService {
+
     AcademicPeriodResponse createAcademicPeriod(CreateAcademicPeriodRequest request);
 
     AcademicPeriodResponse getAcademicPeriodById(Long id);
